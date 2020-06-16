02aaa823e8dd491fc8c59aae77e8bbf94826818a
Victor Tennekes <victor@tennekes.nl>
2020-06-16 12:04:50 +0200

    Fix: resolved #1

--- src/main.c
+++ src/main.c
@@ -5,12 +5,13 @@
 /*                                                     +:+                    */
 /*   By: aaugusti <aaugusti@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/03/23 13:14:58 by aaugusti      #+#   #+#                  */
+/*   Created: 2020/03/23 13:14:58 by aaugusti      #+#    #+#                 */
 /*   Updated: 2020/04/29 19:47:33 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libftprintf.h>
+#include <libft.h>
 #include <minishell.h>
 #include <stdlib.h>
 
@@ -24,6 +25,7 @@ int	main(void)
 	{
 		prompt(&mshell);
 		cmd = get_cmd(&mshell);
-		run_cmd(&mshell, cmd);
+		if (ft_strcmp(cmd, ""))
+			run_cmd(&mshell, cmd);
 	}
 }
