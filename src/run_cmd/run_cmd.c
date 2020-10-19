bafb86a72341aac5891cc4c979257ab559daacc1
Victor Tennekes <victor@tennekes.nl>
2020-10-19 18:06:10 +0200

    Feat: added command piping

--- src/run_cmd/run_cmd.c
+++ src/run_cmd/run_cmd.c
@@ -13,13 +13,49 @@ static void	run_cmd_single(t_mshell *mshell, t_cmd cmd)
 static void	run_cmds(t_mshell *mshell, t_cmd *cmds, size_t cmd_count)
 {
 	size_t	i;
+	int		pfds[2];
+	int		prev_pipe;
+	int		pid;
+	int		std_in;
 
 	i = 0;
-	while (i < cmd_count)
+	prev_pipe = STDIN_FILENO;
+	while (i < cmd_count - 1)
 	{
-		run_cmd_single(mshell, cmds[i]);
+		if (cmds[i].pipe == true)
+		{
+			pipe(pfds);
+			pid = fork();
+			if (pid == 0)
+			{
+				if (prev_pipe != STDIN_FILENO)
+				{
+					dup2(prev_pipe, STDIN_FILENO);
+					close(prev_pipe);
+				}
+				dup2(pfds[1], STDOUT_FILENO);
+				close(pfds[1]);
+				run_cmd_single(mshell, cmds[i]);
+				exit(1);
+			}
+		}
+		if (prev_pipe != STDIN_FILENO)
+			close(prev_pipe);
+		if (cmds[i].pipe == true)
+		{
+			close(pfds[1]);
+			prev_pipe = pfds[0];
+		}
 		i++;
 	}
+	pid = 1;
+	std_in = dup(STDIN_FILENO);
+	if (prev_pipe != STDIN_FILENO) {
+        dup2(prev_pipe, STDIN_FILENO);
+        close(prev_pipe);
+    }
+	run_cmd_single(mshell, cmds[i]);
+	dup2(std_in, STDIN_FILENO);
 }
 
 void		run_cmd(t_mshell *mshell, char *cmd)
